e57dd63cf864278f4903f1f035901253ef3d3b34
DannyGarcia <DannyGarcia@DESKTOP-6PV6HSF>
2019-07-28 02:36:59 -0600

    Se agrego arquitectura basica

---
+++ SoporteTecnico/SoporteTecnico.BL/ServiciosBL.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoporteTecnico.BL
+{
+    public class ServiciosBL
+    {
+       public  List<Servicio> ObtenerServicios()
+        {
+            var servicio1 = new Servicio();
+            servicio1.Id = 1;
+            servicio1.Descripcion = "Mantenimiento Preventivo";
+
+            var servicio2 = new Servicio();
+            servicio2.Id = 2;
+            servicio2.Descripcion = "Reparaciones";
+
+            var servicio3 = new Servicio();
+            servicio3.Id = 3;
+            servicio3.Descripcion = "Instalaciones";
+
+            var servicio4 = new Servicio();
+            servicio4.Id = 4;
+            servicio4.Descripcion = "Diagnosticos";
+
+            var listdeServicios = new List<Servicio>();
+            listdeServicios.Add(servicio1);
+            listdeServicios.Add(servicio2);
+            listdeServicios.Add(servicio3);
+            listdeServicios.Add(servicio4);
+
+            return listdeServicios;
+        }
+    }
+}
--- SoporteTecnico/SoporteTecnico.Web/Controllers/ServiciosController.cs
+++ SoporteTecnico/SoporteTecnico.Web/Controllers/ServiciosController.cs
@@ -1,4 +1,4 @@
-﻿using SoporteTecnico.Web.Models;
+﻿using SoporteTecnico.BL;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,30 +12,10 @@ namespace SoporteTecnico.Web.Controllers
         // GET: Servicios
         public ActionResult Index()
         {
-            var servicio1 = new ServicioModel();
-            servicio1.ID = 1;
-            servicio1.Descripcion = "Mantenimiento Preventivo";
+            var serviciosBL = new ServiciosBL();
+            var listadeServicios = serviciosBL.ObtenerServicios();
 
-            var servicio2 = new ServicioModel();
-            servicio2.ID = 2;
-            servicio2.Descripcion = "Reparaciones";
-
-            var servicio3 = new ServicioModel();
-            servicio3.ID = 3;
-            servicio3.Descripcion = "Instalaciones";
-
-            var servicio4 = new ServicioModel();
-            servicio4.ID = 4;
-            servicio4.Descripcion = "Diagnosticos";
-
-            var listdeServicios = new List<ServicioModel>();
-            listdeServicios.Add(servicio1);
-            listdeServicios.Add(servicio2);
-            listdeServicios.Add(servicio3);
-            listdeServicios.Add(servicio4);
-
-
-            return View(listdeServicios);
+            return View(listadeServicios);
         }
     }
 }
